437ac4542a60cc475bcf6734ea1907b18f163bd4
SergGen <se.gennp@gmail.com>
2021-12-14 18:43:06 +0300

    Написана программа таймера

--- index.js
+++ index.js
@@ -1,56 +1,89 @@
-const colors = require("colors");
+// const colors = require("colors");
+import colors from "colors";
+// const EventEmitter = require('events');
+import {EventEmitter} from 'events';
 
-function numberCheck(paramType, param){
-    if(param === undefined){
-        throw new Error(`${paramType} parameter is not set`);
-    }
-    if(isNaN(Number(param))){
-        throw new Error(`${paramType} parameter is not Number type`);
-    }
+/*
+* Для запуска таймеров указываете параметры времени в следующем формате ГГГГ-ММ-ДД-ЧЧ-ММ-СС
+* */
+
+let inputParams = [...process.argv];
+let workParams = [];
+let timersBlock = [];
+const emitterObject = new EventEmitter();
+
+inputParams.splice(0,2);
+
+if(inputParams.length === 0){
+    throw new Error('Не переданы входные параметры'.red);
 }
 
-numberCheck('Start', process.argv[2]);
-numberCheck('End', process.argv[3]);
+inputParams = inputParams.map(param => param.split('-'));
 
-const startEl = Number(process.argv[2] <= process.argv[3] ? process.argv[2] : process.argv[3]);
-const endEl = Number(process.argv[2] >= process.argv[3] ? process.argv[2] : process.argv[3]);
-let primeNumbers = [];
-let primeNumCheck = true;
-const GREEN = 'green';
-const YELLOW = 'yellow';
-const RED = 'red';
-let chooseColor = GREEN;
+workParams = inputParams.map((param, i) => {
+    let periodBlock = {};
 
-for(let i = startEl; i <= endEl; i++){
-    primeNumCheck = true;
-    for(let j = 2; j <= Math.sqrt(i); j++){
-        if(i % j === 0){
-            primeNumCheck = false;
-            break;
+    if(param.length !== 6) {
+        throw new Error(`Ошибка в переданном параметре ${i + 1}`.red);
+    }
+
+    param.forEach((periodValue, j) => {
+        let periodName = '';
+        if(j === 0){periodName = 'years'}
+        if(j === 1){periodName = 'months'}
+        if(j === 2){periodName = 'days'}
+        if(j === 3){periodName = 'hours'}
+        if(j === 4){periodName = 'minutes'}
+        if(j === 5){periodName = 'seconds'}
+        if(isNaN(Number(periodValue)) || Number(periodValue) < 0) {
+            throw new Error(`Ошибка в переданном параметре ${i + 1} в элементе ${periodName}`.red);
+        }
+        periodBlock[periodName] = Number(periodValue);
+    });
+    return periodBlock;
+})
+
+console.dir(workParams);
+
+class Timers {
+    timerId
+    constructor(timestamp, paramId, emitterObject) {
+        this.paramId = paramId;
+        this.emitterObject = emitterObject;
+        this.timestamp = timestamp - Date.now();
+        this.setTimer();
+    }
+    decreaseTime = () => {
+        if(this.timestamp > 0){
+            this.showCurrentTime();
+            this.timestamp -= 1000;
+        } else {
+            this.stopTimer();
+            console.log('Таймер остановлен');
         }
     }
-    if(primeNumCheck){
-        primeNumbers.push(i);
+    makeTick = () => {
+        this.emitterObject.emit(`start${this.paramId}`)
+    }
+    showCurrentTime() {
+        if(this.timerId % 2 === 0){
+            console.log(colors.yellow(this.timestamp));
+        } else {
+            console.log(colors.green(this.timestamp));
+        }
+
+    }
+    setTimer() {
+        this.timerId = setInterval(this.makeTick, 1000);
+        this.emitterObject.on(`start${this.paramId}`, this.decreaseTime);
+    }
+    stopTimer() {
+        clearInterval(this.timerId);
+        this.emitterObject.removeListener(`start${this.paramId}`, this.decreaseTime);
     }
 }
 
-let outputData = primeNumbers.map((num) => {
-    if(chooseColor === GREEN){
-        chooseColor = YELLOW;
-        return String(num).green;
-    }
-    if(chooseColor === YELLOW){
-        chooseColor = RED;
-        return String(num).yellow;
-    }
-    if(chooseColor === RED){
-        chooseColor = GREEN;
-        return String(num).red;
-    }
+workParams.forEach(({years, months, days, hours, minutes, seconds}, index) => {
+    timersBlock.push(new Timers(Number(new Date(years, months - 1, days, hours, minutes, seconds)),
+        index, emitterObject));
 });
-
-if(primeNumbers.length !== 0) {
-    console.log(outputData.join(' '));
-} else {
-    console.log('Prime numbers not found'.red);
-}
