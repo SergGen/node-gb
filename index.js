a86ac57ce1baab2aff49b6b706b91232ba39936b
SergGen <se.gennp@gmail.com>
2021-12-17 16:00:51 +0300

    В программе для сортировки записей в файлы по заданным IP адресам заменил запись на потоки.

--- index.js
+++ index.js
@@ -7,45 +7,34 @@ import * as readline from 'node:readline';
 
 const regExpIP = /(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/g
 let inputIPs = [...process.argv].join(' ').match(regExpIP) || ['89.123.1.41','34.48.240.111'];
+const readStream = fs.createReadStream('./logs/test.log', 'utf8');
+const readInterface = readline.createInterface({input: readStream});
 
 inputIPs = inputIPs.map(ip => {
-    return {address: ip , regExp: RegExp(ip)};
+    return {address: ip , regExp: RegExp(ip), writeStream: null, statistics: 0};
 });
-
-let statistics = {};
-
-inputIPs.forEach(ip => statistics[ip.address] = 0);
 
 console.dir(inputIPs);
-
-const readStream = fs.createReadStream('./logs/access.log', {
-    encoding: 'utf8',
-    highWaterMark: 512 * 1024
-});
-
-const readInterface = readline.createInterface({
-    input: readStream
-});
-
-const writeFile = (fileName, data) => {
-    fs.appendFile(`./logs/${fileName}_requests.log`, data, (err) => {
-        if (err) throw err;
-    });
-}
 
 readInterface.on('line', line => {
     let foundIP = inputIPs.find(ip => {
         return ip.regExp.test(line);
-    })
-
+    });
     if(foundIP) {
-        writeFile(foundIP.address, line + '\n');
-        statistics[foundIP.address]++;
+        if(!foundIP.writeStream) {
+            foundIP.writeStream = fs.createWriteStream(`./logs/${foundIP.address}_requests.log`,
+                {flags: 'a', autoClose: true});
+        }
+        foundIP.writeStream.write(line + '\n');
+        foundIP.statistics++;
     }
 });
+
+console.dir(readStream);
+
 readStream.on('open', () => console.log(`File reading started at ${(new Date()).toTimeString()}`));
 readStream.on('end', () => {
     console.log(`File reading finished at ${(new Date()).toTimeString()}`);
-    console.dir(statistics);
+    inputIPs.forEach(ip => console.log(ip.address, '\t', ip.statistics));
 });
 readStream.on('error', (err) => console.log(err));
